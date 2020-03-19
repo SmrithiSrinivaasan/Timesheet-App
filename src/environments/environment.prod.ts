c250d5db5d9b594a4eb2a0f23ebd89a005a5aedc
SmrithiSrinivaasan <smrithi2006@gmail.com>
2020-03-19 19:27:06 +0530

    Quality Check

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyAzv3NbYYsDnFeydi1Tx6ALI2IWXkQcYN0',
+    authDomain: 'timesheet-d3dea.firebaseapp.com',
+    databaseURL: 'https://timesheet-d3dea.firebaseio.com',
+    projectId: 'timesheet-d3dea',
+    storageBucket: 'timesheet-d3dea.appspot.com',
+    messagingSenderId: '27911365185',
+    appId: '1:27911365185:web:c80139f19687d643cfdcc0',
+    measurementId: 'G-WQRGKCW766',
+  },
 };
