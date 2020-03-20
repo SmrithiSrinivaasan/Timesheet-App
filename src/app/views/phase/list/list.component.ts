e1127237a5adb05436b83120ff14833ed316cdb9
SmrithiSrinivaasan <smrithi2006@gmail.com>
2020-03-20 13:05:27 +0530

    16-Phase-List-Page-Design

--- src/app/views/phase/list/list.component.ts
+++ src/app/views/phase/list/list.component.ts
@@ -6,7 +6,7 @@ import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css'],
+  // styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
   phases = [];
